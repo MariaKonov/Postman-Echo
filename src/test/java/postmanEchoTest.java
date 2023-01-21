dfa757ecaace635b94bcfd3a572933a6c04e2c54
Maria <marijka000@gmail.com>
2023-01-21 18:57:41 +0300

    postmanEcho

---
+++ src/test/java/postmanEchoTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanEchoTest {
+
+    @Test
+    void shouldTestSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldTestPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
+
+
